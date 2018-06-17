c17903fdfcf742a95bf5752c60aa53f1c38b5705
sl <sl@computas.com>
2018-06-17 12:41:26 +0200

    Added a basic create-react-app starter

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>JavaZone 2019</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
