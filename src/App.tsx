8de199725c04b5aabbc9e00d79ba54c8a6854f15
sl <sl@computas.com>
2018-12-13 19:06:18 +0100

    Added temp pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,34 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navigation, NavRoute } from './components/Navigation/Navigation';
 import { Frontpage } from './pages/Frontpage/Frontpage';
 import { Footer } from './components/Footer/Footer';
+import { Info } from './pages/Info/Info';
+import { Tickets } from './pages/Tickets/Tickets';
+import { Partners } from './pages/Partners/Partners';
+import { Academy } from './pages/Academy/Academy';
+import { Kids } from './pages/Kids/Kids';
 
 const routes: NavRoute[] = [
   {title: 'Info', url: '/info'},
   {title: 'Tickets', url: '/tickets'},
   {title: 'Partners', url: '/partners'},
-  {title: 'Academy', url: '/partners'},
-  {title: 'Kids', url: '/partners'},
-  {title: 'About', url: '/partners'}
+  {title: 'Academy', url: '/academy'},
+  {title: 'Kids', url: '/kids'},
 ];
 
 class App extends React.Component {
   public render() {
     return (
       <Router>
-        <div>
+        <>
           <Navigation sticky routes={routes} />
           <Route exact path="/" component={Frontpage} />
+          <Route exact path="/info" component={Info} />
+          <Route exact path="/tickets" component={Tickets} />
+          <Route exact path="/partners" component={Partners} />
+          <Route exact path="/academy" component={Academy} />
+          <Route exact path="/kids" component={Kids} />
           <Footer />
-        </div>
+        </>
       </Router>
     );
   }
--- src/pages/Frontpage/Frontpage.tsx
+++ src/pages/Frontpage/Frontpage.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { Jumbotron } from '../../components/Jumbotron/Jumbotron';
 
-export class Frontpage extends React.Component<{}, {}> {
-    public render() {
-        return (
-            <div>
-                <Jumbotron title="JavaZone 2019" subTitle="See you next year!" />
-            </div>
-        )
-    }
+export function Frontpage() {
+    return (
+        <>
+            <Jumbotron title="JavaZone 2019" subTitle="See you next year!" />
+        </>
+    )
 }
