977aa019ef3a834d8f665e7cceedda9ddaddea2a
Rustam Mehmandarov <mehmandarov@gmail.com>
2019-01-14 23:36:28 +0100

    Minro update to the frontpage

--- src/pages/Frontpage/Frontpage.tsx
+++ src/pages/Frontpage/Frontpage.tsx
@@ -4,7 +4,7 @@ import { Jumbotron } from '../../components/Jumbotron/Jumbotron';
 export function Frontpage() {
     return (
         <>
-            <Jumbotron title="JavaZone 2019" subTitle="September 11th - 12th, 2019" />
+            <Jumbotron title="JavaZone 2019" subTitle="September 11th - 12th" />
         </>
     )
 }
