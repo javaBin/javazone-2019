8c1bc86437e6c58120798111ae294b242300f9ab
Rustam Mehmandarov <mehmandarov@gmail.com>
2019-01-14 17:31:42 +0100

    Added date to the frontpage. Yay.

--- src/pages/Frontpage/Frontpage.tsx
+++ src/pages/Frontpage/Frontpage.tsx
@@ -4,7 +4,7 @@ import { Jumbotron } from '../../components/Jumbotron/Jumbotron';
 export function Frontpage() {
     return (
         <>
-            <Jumbotron title="JavaZone 2019" subTitle="See you this year!" />
+            <Jumbotron title="JavaZone 2019" subTitle="September 11th - 12th, 2019" />
         </>
     )
 }
