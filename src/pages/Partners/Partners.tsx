3c22cc9e0c6fb5265c71856be6f31d780200ae5e
Rustam Mehmandarov <mehmandarov@gmail.com>
2019-01-16 00:49:44 +0100

    Content update for partners' page

--- src/pages/Partners/Partners.tsx
+++ src/pages/Partners/Partners.tsx
@@ -22,27 +22,27 @@ export function Partners() {
                     JavaZone started out in 2002 as a small conference for Java developers but has since then evolved to become one of the largest developer conferences in the world. Each year thousands of people gather in Oslo Spektrum for three full days of learning, mingling, and fun!
                 </p>
             </CenterSection>
-            <Section header={<h1>Last two years: Completely. Sold. Out.</h1>}>
+            <Section header={<h1>Last three years: Completely. Sold. Out.</h1>}>
                 <p>
-                    We want JavaZone‘s Expo Area to be an attractive space where our Partners and participants can hang out and spend time together. In previous years our Partners have done a great job to help us achieve this goal, and we look forward to seeing what they bring to JavaZone 2018! JavaZone’s Expo Area is a vibrant, busy space full of life and activity. As a Partner you are not just supporting our conference economically – you participate in the event and help create a unique atmosphere. We work closely with our Partners to help them realize their ideas and make the most out of their stands. Have you got a great concept for your JavaZone stand and need help to realize it? Don’t be shy - just ask us. Almost anything is possible!
+                    Last year, a record breaking 3100 people attended JavaZone. We were completely sold out of tickets AND partner stands. The same happened last year, and a year before. So you might want to join in early if you want to be part of the conference!
                 </p>
             </Section>
             <Section header={<h1>Out philosophy: Partners, not sponsors</h1>}>
                 <p>
-                    We want JavaZone‘s Expo Area to be an attractive space where our Partners and participants can hang out and spend time together. In previous years our Partners have done a great job to help us achieve this goal, and we look forward to seeing what they bring to JavaZone 2018! JavaZone’s Expo Area is a vibrant, busy space full of life and activity. As a Partner you are not just supporting our conference economically – you participate in the event and help create a unique atmosphere. We work closely with our Partners to help them realize their ideas and make the most out of their stands. Have you got a great concept for your JavaZone stand and need help to realize it? Don’t be shy - just ask us. Almost anything is possible!
+                    We want JavaZone‘s Expo Area to be an attractive space where our partners and participants can hang out and spend time together. In previous years, our partners have done a great job helping us achieve this goal, and we look forward to seeing what they bring to JavaZone 2019! JavaZone’s Expo Area is a vibrant, busy space full of life and activity. As a partner you are not just supporting our conference economically – you participate in the event and help creating a unique atmosphere. We work closely with our partners to help them realize their ideas and make the most out of their stands. Have you got a great concept for your JavaZone stand and need help to realize it? Don’t be shy - just ask us. Almost anything is possible!
                 </p>
             </Section>
             <ImageSection bottom imageName="2018/180912_JavaZone_0098" />
             <Seperator />
             <CenterSection header={<h1>The Expo Area</h1>}>
                 <p>
-                    All our partner stands are traditionally located in the Expo Area, which is centrally located in the venue. During each break, the Expo is full of attendees. This is partly because we serve food continuously from restaurant stands throughout the day, and partly because of you – our Partners – make this area one of the liveliest Expo Areas of any conference.
+                    All our partner stands are traditionally located in the Expo Area, which is located in the center of the venue. During each break, the Expo is full of attendees. This is partly because we continuously serve food from the restaurant stands – throughout the day, and partly because of you – our partners – make this area one of the liveliest Expo Areas of any conference.
                 </p>
                 <p>
-                    Don‘t you believe us? Have a look for yourself! Our new Partners are always surprised by the huge attention their stands receive in the Expo Area. We have put together a short 15-second timelapse from 2 hours of Expo time, showing both the flow of people during talks and in the breaks between talks. Yes, you get exposure to that many people, for a total of 20 hours!
+                    Don‘t you believe us? Have a look for yourself! Our new partners are always surprised by the huge attention their stands receive in the Expo Area. We have put together a short 15-second timelapse from 2 hours of Expo time, showing both the flow of people during talks and in the breaks between talks. Yes, you get exposure to that many people, for a total of 20 hours!
                 </p>
                 <p>
-                    <i>The video below shows a time lapse of the JavaZone 2018 event.</i>
+                    <i>The video below shows a time-lapse of the JavaZone 2018 event.</i>
                 </p>
             </CenterSection>
             <CenterSection>
@@ -55,11 +55,11 @@ export function Partners() {
             </CenterSection>
             <Section header={<h1>Partner Options</h1>}>
                 <p>
-                    Even though all Partners are considered equal, there are a few options to choose from. There is a base package that everyone gets, and then there are a few available expansions. Notice that these expansions have a limited availability.
+                    Even though all partners are considered equal, there are a few options to choose from. There is a base package that everyone gets, and then there are a few available expansions. Note that these expansions have limited availability.
                 </p>
                 <h2>Included in all partnerships</h2>
                 <List>
-                    <li>A 6 square meter stand space in the central arena at Oslo Spektrum with two side walls.</li>
+                    <li>A 6-square meter stand space in the central arena at Oslo Spektrum with two side walls.</li>
                     <li>4 tickets to the conference for manning the stand.</li>
                     <li>Discounted participant tickets for your employees.</li>
                     <li>Branding on <InlineLink external url="https://www.javazone.no">javazone.no</InlineLink>, at the venue, the program and in other promotional material that JavaZone produces.</li>
@@ -74,27 +74,27 @@ export function Partners() {
                 <List>
                     <li>A dedicated food stand serving food throughout the conference. The menu will be decided in collaboration between yourselves and our event partners.</li>
                     <li>Approx. 108 square meters (12 x 9).</li>
-                    <li>4 meter high back wall.</li>
+                    <li>4-meter high back wall.</li>
                 </List>
                 <h4>Concept Stand</h4>
                 <List>
                     <li>A large space for presenting your own 'concept'. This could be, for example, a gaming area, a coffee bar, or a chillout area.</li>
                     <li>Approx. 70 square meters (10 x 7).</li>
-                    <li>4 meter high back wall.</li>
+                    <li>4-meter high back wall.</li>
                 </List>
             </Section>
             <Seperator />
             <CenterSection header={<h1>Partnership prices</h1>}>
                 <TicketPrice>71.000</TicketPrice>
                 <p>
-                    <i>Note that all 50 partnerships for JavaZone 2018 were sold out long before the conference.</i>
+                    <i>Note that all 50+ partnerships for JavaZone 2018 were sold out long before the conference.</i>
                 </p>
                 <p>
                     All partnerships have a base price for entry which gives you everything described above.
-                    We also have some add ons, as well as tickets for your employees, priced below.
+                    We also have some add-ons, as well as tickets for your employees, priced below.
                 </p>
                 <p>
-                    <i style={{fontSize: '1.2rem'}}>After 1st of March the price will increase to NOK 86.000,-.</i><br />
+                    <i style={{fontSize: '1.2rem'}}>After March 1st, the price will increase to NOK 86.000,-.</i><br />
                     <i style={{fontSize: '1rem'}}>All prices are ex VAT.</i>
                 </p>
             </CenterSection>
@@ -108,10 +108,10 @@ export function Partners() {
             </Section>
             <Section header={<h1>Partnership ticket</h1>}>
                 <PartnerPriceList>
-                    <PartnerPrice item="Tickets for employees (0 - 30)">5.590</PartnerPrice>
-                    <PartnerPrice item="Tickets for employees (31 - 50)">5.390</PartnerPrice>
-                    <PartnerPrice item="Tickets for employees (51 - ∞)">5.190</PartnerPrice>
-                    <PartnerPrice item="Tickets for employees (Late-Bird)">6.190</PartnerPrice>
+                    <PartnerPrice item="Tickets for employees (0 - 30)">5.890</PartnerPrice>
+                    <PartnerPrice item="Tickets for employees (31 - 50)">5.690</PartnerPrice>
+                    <PartnerPrice item="Tickets for employees (51 - ∞)">5.490</PartnerPrice>
+                    <PartnerPrice item="Tickets for employees (Late-Bird)">6.490</PartnerPrice>
                 </PartnerPriceList>
             </Section>
             <ImageSection imageName="2018/180912_JavaZone_0158" />
