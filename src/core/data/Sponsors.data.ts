d855e7c17032652fee9ba0f4d35a913b0007b4d1
Steffen Lien <steffe.lien@ehelse.no>
2019-01-23 09:11:35 +0100

    Build failed, lets try again

--- src/core/data/Sponsors.data.ts
+++ src/core/data/Sponsors.data.ts
@@ -1,3 +1,28 @@
-const sponsors = [
-    
+import { Sponsor } from '../models';
+
+export const sponsors: Sponsor[] = [
+    {
+        name: 'Vipps',
+        homepageUrl: 'https://www.vipps.no/'
+    },
+    {
+        name: 'Sysco',
+        homepageUrl: 'https://sysco.no/'
+    },
+    {
+        name: 'Sportradar',
+        homepageUrl: 'https://www.sportradar.com/'
+    },
+    {
+        name: 'Tripletex',
+        homepageUrl: 'https://www.tripletex.no/'
+    },
+    {
+        name: 'Kantega',
+        homepageUrl: 'https://www.kantega.no/'
+    },
+    {
+        name: 'Netcompany',
+        homepageUrl: 'https://www.netcompany.com/no'
+    }
 ]
---
+++ src/core/models/Sponsor.model.ts
@@ -0,0 +1,4 @@
+export interface Sponsor {
+    name: string;
+    homepageUrl: string;
+}
