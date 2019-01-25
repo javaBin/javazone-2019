c055a1488aec69c4ea326e549bf0070b715eafbf
Rustam Mehmandarov <mehmandarov@gmail.com>
2019-01-25 14:46:09 +0100

    Added cookie information

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import Footer from './components/Footer/Footer';
 import { InfoPage } from './pages/Info/Info';
 import { PartnersPage } from './pages/Partners/Partners';
 import { AcademyPage } from './pages/Academy/Academy';
+import { CookieInfoPage } from './pages/CookieInfoPage/CookieInfoPage';
 import { CookieDisclaimer } from './components/CookieDisclaimer/CookieDisclaimer';
+import { InlineLink } from './components/InlineLink/InlineLink';
 import { NotFoundPage } from './pages/NotFound/NotFound';
 
 const routes: NavRoute[] = [
   {title: 'Info', url: '/info'},
   {title: 'Partners', url: '/partners'},
   {title: 'Academy', url: '/academy'},
+  {title: 'Cookies', url: '/privacy-cookies'},
 ];
 
 const ScrollToTop = () => {
@@ -33,11 +36,12 @@ class App extends React.Component {
             {/* <Route exact path="/tickets" component={Tickets} /> */}
             <Route exact path="/partners" component={PartnersPage} />
             <Route exact path="/academy" component={AcademyPage} />
+            <Route exact path="/privacy-cookies" component={CookieInfoPage} />
             <Route component={NotFoundPage} />
           </Switch>
           <Footer routes={routes} />
           <CookieDisclaimer>
-            We have given you a dedicated cookie from our cookie jar in order to store your favorite program schedule. We also use cookies to improve your experience on our site. By using this site you agree to this.
+            We have given you a dedicated cookie from our cookie jar in order to store your favorite program schedule. We also use cookies to improve your experience on our site. By using this site you agree to this. For more information please visit our <InlineLink url="/privacy-cookies">cookie information page.</InlineLink>
           </CookieDisclaimer>
         </>
       </Router>
