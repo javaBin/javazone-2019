0cd77bf35904c86f51cffd0cba2dd67b55543446
Steffen Lien <steffe.lien@ehelse.no>
2019-03-10 16:16:51 +0100

    starting the major overhaul

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Jumbotron } from '../../components/Jumbotron/Jumbotron';
+import { Jumbotron } from '../components/Jumbotron/Jumbotron';
 
-export function FrontPage() {
+function FrontPage() {
     return (
         <>
             <Jumbotron title="JavaZone 2019" subTitle="September 11th - 12th" location="Oslo Spektrum" />
         </>
     )
 }
+
+
+export default FrontPage;
