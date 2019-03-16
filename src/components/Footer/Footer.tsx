a340ab87a53a38838c5eb8cbcc73b67698bebccb
lzpat <patriciz@stud.ntnu.no>
2019-03-16 16:15:34 +0100

    removed frontpage footer scss

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,48 +1,43 @@
-import React from 'react';
-import styles from './Footer.module.scss';
-import { useIsFrontpage } from '../../core/hooks/UseIsFrontpage';
-import { RouteComponentProps, withRouter } from 'react-router';
-import classnames from 'classnames';
-import { NavRoute } from '../Navigation/Navigation';
+import React from "react";
+import styles from "./Footer.module.scss";
+import { useIsFrontpage } from "../../core/hooks/UseIsFrontpage";
+import { RouteComponentProps, withRouter } from "react-router";
+import classnames from "classnames";
+import { NavRoute } from "../Navigation/Navigation";
 
 interface FooterProps extends RouteComponentProps {
-    routes: NavRoute[];
+  routes: NavRoute[];
 }
 
 function Footer(props: FooterProps) {
-    const isFrontpage = useIsFrontpage(props.location.pathname, props.routes);
-    const componentClass = classnames(
-        styles.footer,
-        {[styles.frontpage]: isFrontpage},
-    );
+  const isFrontpage = useIsFrontpage(props.location.pathname, props.routes);
+  const componentClass = classnames(styles.footer);
 
-    if(isFrontpage){
-        return null;
-    }
-    else{
+  if (isFrontpage) {
+    return null;
+  } else {
     return (
-        <footer className={componentClass}>
-            <div className={styles.javabin}>
-                <a target="_blank" href="https://javabin.no">
-                    Made by javaBin
-                </a>
-                <a target="_blank" href="https://java.no/principles">
-                    Code of Conduct
-                </a>
-            </div>
-            <div className={styles.social}>
-                <a href="mailto:javazone@java.no">
-                    Mail
-                </a>
-                <a target="_blank" href="https://twitter.com/javazone">
-                    Twitter
-                </a>
-                <a target="_blank" href="https://facebook.com/javazoneconference">
-                    Facebook
-                </a>
-            </div>
-        </footer>
-    )}
+      <footer className={componentClass}>
+        <div className={styles.javabin}>
+          <a target="_blank" href="https://javabin.no">
+            Made by javaBin
+          </a>
+          <a target="_blank" href="https://java.no/principles">
+            Code of Conduct
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a href="mailto:javazone@java.no">Mail</a>
+          <a target="_blank" href="https://twitter.com/javazone">
+            Twitter
+          </a>
+          <a target="_blank" href="https://facebook.com/javazoneconference">
+            Facebook
+          </a>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default withRouter(Footer);
