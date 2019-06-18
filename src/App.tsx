1e20cfcda0d3d69fb40fa79ca0d8f6a2497315ee
Mateusz <mz@computas.com>
2019-06-18 16:08:44 +0200

    Academy fall

--- src/App.tsx
+++ src/App.tsx
@@ -18,22 +18,25 @@ import { NotFoundPage } from './pages/NotFound/NotFound';
 import { CookieInfoPage } from './pages/CookieInfo/CookieInfo';
 import Index from './pages/Frontpage/Frontpage';
 import theme from './core/theme';
-import {ProgramPage} from "./pages/program/Program";
-import {ProgramDetailsPage} from "./pages/program/ProgramDetails";
+import { ProgramPage } from './pages/program/Program';
+import { ProgramDetailsPage } from './pages/program/ProgramDetails';
+import { AcademyAutumnPage } from './pages/AcademyAutumn/AcademyAutumn';
+
 
 const routes: NavRoute[] = [
-  {title: 'Info', url: '/info', show: true},
-  {title: 'Program', url: '/program', show: true},
-  {title: 'Tickets', url: '/tickets', show: true},
-  {title: 'Speakers', url: '/speakers', show: true},
-  {title: 'Partners', url: '/partners', show: true},
-  {title: 'Academy', url: '/academy', show: false},
-  {title: 'Kids', url: '/kids', show: false},
-  {title: 'Frivillige', url: '/frivillige', show: true},
-  {title: 'About', url: '/about', show: true},
-  {title: 'Cookies', url: '/privacy-cookies', show: false},
-  {title: 'Tips for Speakers', url: '/speakers/tips', show: false},
-  {title: 'Monetary Policy', url: '/speakers/monetary-policy', show: false},
+  { title: 'Info', url: '/info', show: true },
+  { title: 'Program', url: '/program', show: true },
+  { title: 'Tickets', url: '/tickets', show: true },
+  { title: 'Speakers', url: '/speakers', show: true },
+  { title: 'Partners', url: '/partners', show: true },
+  { title: 'Academy', url: '/academy', show: false },
+  { title: 'Academy', url: '/academy', show: true },
+  { title: 'Kids', url: '/kids', show: false },
+  { title: 'Frivillige', url: '/frivillige', show: true },
+  { title: 'About', url: '/about', show: true },
+  { title: 'Cookies', url: '/privacy-cookies', show: false },
+  { title: 'Tips for Speakers', url: '/speakers/tips', show: false },
+  { title: 'Monetary Policy', url: '/speakers/monetary-policy', show: false },
 ];
 
 const ScrollToTop = () => {
@@ -53,7 +56,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <>
           <Navigation routes={routes} />
-          <Route component={ScrollToTop}/>
+          <Route component={ScrollToTop} />
           <Switch>
             <Route exact path="/" component={Index} />
             <Route exact path="/info" component={InfoPage} />
@@ -63,6 +66,7 @@ function App() {
             <Route exact path="/speakers" component={SpeakersPage} />
             <Route exact path="/partners" component={PartnersPage} />
             <Route exact path="/frivillige" component={FrivilligePage} />
+            <Route exact path="/academy" component={AcademyAutumnPage} />
             {/* <Route exact path="/academy" component={AcademyPage} /> */}
             {/* <Route exact path="/kids" component={KidsPage} /> */}
             <Route exact path="/about" component={AboutUsPage} />
