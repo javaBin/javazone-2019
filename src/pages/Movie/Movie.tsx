d42133871f35431eb426284431cfbcc0c805c700
alexander-svendsen <alexandersvendsen1990@gmail.com>
2019-08-18 16:47:05 +0200

    Change movie header

--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -22,7 +22,7 @@ export function MoviePage() {
 
     return (
         <>
-            <PageBanner header="Movie Time!" subHeader="The J-team" color={themeColor} artPath={pageArt}/>
+            <PageBanner header="The J-team" subHeader="Movie Time" color={themeColor} artPath={pageArt}/>
             <MovieContainer>
                 <iframe id={styles.youtube}
                         title="youtube movie"
