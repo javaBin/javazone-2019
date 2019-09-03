29c228bc953b0e28272e5505603b0f54c28a920c
Rustam Mehmandarov <mehmandarov@gmail.com>
2019-09-03 13:59:35 +0200

    Moving pixelsss. v.2.0

--- src/pages/MobileApp/MobileAppPage.tsx
+++ src/pages/MobileApp/MobileAppPage.tsx
@@ -21,7 +21,7 @@ export function MobileAppPage() {
                     </p>
                     <div className={styles.badges}>
                         <a href="https://apps.apple.com/no/app/javazone/id384888388?mt=8" style={{display:"inline-block", overflow:"hidden", background:"url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2010-08-22&kind=iossoftware&bubble=ios_apps) no-repeat", width:"135px", height:"40px"}}></a>
-                        <a href='https://play.google.com/store/apps/details?id=no.schedule.javazone.v3&hl=en&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img style={{height:"60px", paddingTop:"2px"}} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                        <a href='https://play.google.com/store/apps/details?id=no.schedule.javazone.v3&hl=en&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img style={{height:"59px", paddingTop:"2px"}} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
                     </div>
                 </TextBlock>
                 <TextBlock color={themeColor} title="Digital Pass">
